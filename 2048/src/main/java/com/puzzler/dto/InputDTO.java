bffa3a26f0cc3621b6e713f84069d63da4d3bbce
Yuvan Shankar S <yuvanshankars.aiml2023@citchennai.net>
2026-08-03 00:21:45 +0530

    feat: input dto

---
+++ 2048/src/main/java/com/puzzler/dto/InputDTO.java
@@ -0,0 +1,15 @@
+package com.puzzler.dto;
+
+public class InputDTO {
+
+  private char move;
+
+  public InputDTO (char move) {
+    this.move = move;
+  }
+
+  public char getMove() {
+    return move;
+  }
+
+}
