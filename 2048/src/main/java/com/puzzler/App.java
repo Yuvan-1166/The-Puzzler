22922b66d428147ed5fc3bed555389b21c06ecda
Yuvan Shankar S <yuvanshankars.aiml2023@citchennai.net>
2026-08-03 01:32:23 +0530

    fix: removed ambigous I/O

--- 2048/src/main/java/com/puzzler/App.java
+++ 2048/src/main/java/com/puzzler/App.java
@@ -2,8 +2,6 @@ package com.puzzler;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("The Puzzler!");
-
         Game game = new Game();
         game.gameLoop();
     }
